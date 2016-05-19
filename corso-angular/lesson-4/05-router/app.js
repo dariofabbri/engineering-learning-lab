e7d62b2996b530bca3030e93955f85c672dd3336
Dario Fabbri <dar.fabbri@gmail.com>
2016-05-19 19:20:18 +0200

    Fixed syntax for dependency injection.

--- corso-angular/lesson-4/05-router/app.js
+++ corso-angular/lesson-4/05-router/app.js
@@ -2,31 +2,37 @@
 	'use strict';
 
 	angular.module('app', ['ui.router'])
-		.config(function ($stateProvider, $urlRouterProvider) {
+		.config([
+			'$stateProvider', 
+			'$urlRouterProvider', 
+			function ($stateProvider, $urlRouterProvider) {
 
-			// The default state is 'home'.
-			//
-			$urlRouterProvider.otherwise("/home");
+				// The default state is 'home'.
+				//
+				$urlRouterProvider.otherwise("/home");
 
-			$stateProvider
-				.state('home', {
-					url: "/home",
-					templateUrl: "home/home.html"
-				})
-				.state('contacts', {
-					url: "/contacts",
-					templateUrl: "contacts/list.html",
-					controller: 'ContactsListCtrl as ctrl'
-				})
-				.state('contacts.detail', {
-					url: "/{id:int}",
-					templateUrl: "contacts/detail.html",
-					controller: 'ContactDetailCtrl as ctrl'
-				})
-				.state('contacts.edit', {
-					url: "/{id:int}/edit",
-					templateUrl: "contacts/edit.html",
-					controller: 'ContactDetailCtrl as ctrl'
-				})
-		});
+				// Routes configuration.
+				// 
+				$stateProvider
+					.state('home', {
+						url: "/home",
+						templateUrl: "home/home.html"
+					})
+					.state('contacts', {
+						url: "/contacts",
+						templateUrl: "contacts/list.html",
+						controller: 'ContactsListCtrl as ctrl'
+					})
+					.state('contacts.detail', {
+						url: "/{id:int}",
+						templateUrl: "contacts/detail.html",
+						controller: 'ContactDetailCtrl as ctrl'
+					})
+					.state('contacts.edit', {
+						url: "/{id:int}/edit",
+						templateUrl: "contacts/edit.html",
+						controller: 'ContactDetailCtrl as ctrl'
+					});
+
+		}]);
 }());
--- corso-angular/lesson-4/05-router/contacts/detailController.js
+++ corso-angular/lesson-4/05-router/contacts/detailController.js
@@ -2,18 +2,21 @@
 	'use strict';
 
 	angular.module('app')
-		.controller('ContactDetailCtrl', function ($stateParams, $rootScope) {
-			
-			var me = this;
-
-			for (var i = 0; i < $rootScope.contacts.length; i+= 1) {
+		.controller('ContactDetailCtrl', [
+			'$stateParams', 
+			'$rootScope', 
+			function ($stateParams, $rootScope) {
 				
-				if ($rootScope.contacts[i].id === $stateParams.id) {
-					me.contact = $rootScope.contacts[i];
-					break;
-				}
-			}
+				var me = this;
 
-		});
+				for (var i = 0; i < $rootScope.contacts.length; i+= 1) {
+					
+					if ($rootScope.contacts[i].id === $stateParams.id) {
+						me.contact = $rootScope.contacts[i];
+						break;
+					}
+				}
+
+		}]);
 }());
 
