bb4c7d7e4bc714b2e1b35e79720319ee2888aefb
Dario Fabbri <dar.fabbri@gmail.com>
2016-06-11 15:44:29 +0200

    Completed REST interface for events.

--- corso-angular/express-test/routes/events.js
+++ corso-angular/express-test/routes/events.js
@@ -5,9 +5,13 @@ var db = require('../db');
 router.get('/', function(req, res) {
 	
 	var collection = db.get().collection('events');
-	collection.find().toArray(function (err, docs) {
-		res.json(docs);
-	});
+	collection.find(
+		{}, 
+		{fields: {_id: 0}})
+		.toArray(
+			function (err, docs) {
+				res.json(docs);
+			});
 });
 
 router.get('/:id', function(req, res) {
@@ -16,9 +20,9 @@ router.get('/:id', function(req, res) {
 	collection.findOne(
 		{id: parseInt(req.params.id)},
 		{fields: {_id: 0}},
-		function (err, item) {
-			if (item) {
-				res.json(item);
+		function (err, doc) {
+			if (doc) {
+				res.json(doc);
 			} else {
 				res.status(404).json({error: 'Event not found.'});
 			}
@@ -29,25 +33,65 @@ router.get('/:id', function(req, res) {
 router.post('/', function(req, res) {
 
 	var collection = db.get().collection('events');
-	collection.insertOne(req.body, function (err, doc) {
-		res.json(doc);
-	});
+
+	collection.findOne(
+		{id: parseInt(req.body.id)},
+		function (err, doc) {
+			if (doc) {
+				res.status(409).json({error: 'Event already present.'});
+			} else {
+				collection.insertOne(req.body, function (err, doc) {
+					if(err) {
+						res.status(500).json(err);
+					} else {
+						delete(req.body._id);
+						res.json(req.body);
+					}
+				});
+			}
+		} 
+	);
 });
 
 router.put('/:id', function(req, res) {
-	res.send('Event updated');
+
+	var collection = db.get().collection('events');
+	
+	if(parseInt(req.params.id) !== req.body.id) {
+		res.status(400).json({error: 'id in body does not match id in path parameter.'});
+		return;
+	}
+
+	collection.findOneAndUpdate(
+		{id: parseInt(req.params.id)},
+		req.body,
+		{
+			projection: { _id: 0 }
+		},
+		function (err, doc) {
+			if(doc) {
+				res.json(doc.value);
+			} else {
+				res.status(404).json({error: 'Event not found.'});
+			}
+		});
 });
 
 router.delete('/:id', function(req, res) {
 
 	var collection = db.get().collection('events');
-	collection.findOneAndDelete({id: parseInt(req.params.id)}).then(function (item) {
-		if(item) {
-			res.json(item.value);
-		} else {
-			res.status(404).json({error: 'Event not found.'});
-		}
-	});
+	collection.findOneAndDelete(
+		{id: parseInt(req.params.id)},
+		{
+			projection: { _id: 0 }
+		},
+		function (err, doc) {
+			if(doc) {
+				res.json(doc.value);
+			} else {
+				res.status(404).json({error: 'Event not found.'});
+			}
+		});
 });
 
 module.exports = router;
